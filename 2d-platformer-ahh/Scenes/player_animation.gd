abc19d884e2a4efeacfe4fc08db96f69654663cb
Molkly <mlawvacek@gmail.com>
2025-04-12 23:24:52 -0700

    more

--- 2d-platformer-ahh/Scenes/player_animation.gd
+++ 2d-platformer-ahh/Scenes/player_animation.gd
@@ -10,7 +10,12 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-
+	if player_controller.direction == 1:
+		var right = true
+		var left = false
+	if player_controller.direction == -1:
+		var left = true
+		var right = false
 #flip sprite
 	if player_controller.direction == 1:
 		sprite.flip_h = false
@@ -18,13 +23,19 @@ func _process(delta: float) -> void:
 		sprite.flip_h = true
 
 #plays move animation
-	if abs(player_controller.velocity.x) > 0.0:
-		animation_player.play("move")
-	else:
+	if abs(player_controller.velocity.x) > 0.0 and player_controller.direction == 1:
+		animation_player.play("move_right")
+	elif abs(player_controller.velocity.x) > 0.0 and player_controller.direction == -1:
+		animation_player.play("move_left")
+	elif player_controller.velocity.x == 0:
 		animation_player.play("Idle")
 
 #plays jump animation
-	if player_controller.velocity.y < 0.0:
-		animation_player.play("jump")
-	elif player_controller.velocity.y > 0.0:
-		animation_player.play("fall")
+	if player_controller.velocity.y < 0.0 and player_controller.direction == 1:
+		animation_player.play("jump_right")
+	elif player_controller.velocity.y < 0.0 and player_controller.direction == -1:
+		animation_player.play("jump_left")
+	elif player_controller.velocity.y > 0.0 and player_controller.direction == 1:
+		animation_player.play("fall_right")
+	elif player_controller.velocity.y > 0.0 and player_controller.direction == -1:
+		animation_player.play("fall_left")
