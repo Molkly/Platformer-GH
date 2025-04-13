20fd3bb254e0af6842d77401617c2ca5984f0173
Molkly <mlawvacek@gmail.com>
2025-04-13 01:50:24 -0700

    robot fixes

--- 2d-platformer-ahh/Scenes/player_animation.gd
+++ 2d-platformer-ahh/Scenes/player_animation.gd
@@ -10,12 +10,6 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	if player_controller.direction == 1:
-		var right = true
-		var left = false
-	if player_controller.direction == -1:
-		var left = true
-		var right = false
 #flip sprite
 	if player_controller.direction == 1:
 		sprite.flip_h = false
@@ -23,19 +17,21 @@ func _process(delta: float) -> void:
 		sprite.flip_h = true
 
 #plays move animation
-	if abs(player_controller.velocity.x) > 0.0 and player_controller.direction == 1:
+	if abs(player_controller.velocity.x) > 0.0 and player_controller.right == true:
 		animation_player.play("move_right")
-	elif abs(player_controller.velocity.x) > 0.0 and player_controller.direction == -1:
+	elif abs(player_controller.velocity.x) > 0.0 and player_controller.left == true:
 		animation_player.play("move_left")
-	elif player_controller.velocity.x == 0:
-		animation_player.play("Idle")
-
+	elif player_controller.velocity.x == 0 and player_controller.right == true:
+		animation_player.play("idle_right")
+	elif player_controller.velocity.x == 0 and player_controller.left == true:
+		animation_player.play("idle_left")
 #plays jump animation
-	if player_controller.velocity.y < 0.0 and player_controller.direction == 1:
+	if player_controller.velocity.y < 0.0 and player_controller.right == true:
 		animation_player.play("jump_right")
-	elif player_controller.velocity.y < 0.0 and player_controller.direction == -1:
+	elif player_controller.velocity.y < 0.0 and player_controller.left == true:
 		animation_player.play("jump_left")
-	elif player_controller.velocity.y > 0.0 and player_controller.direction == 1:
+
+	if player_controller.velocity.y > 0.0 and player_controller.right == true:
 		animation_player.play("fall_right")
-	elif player_controller.velocity.y > 0.0 and player_controller.direction == -1:
+	elif player_controller.velocity.y > 0.0 and player_controller.left == true:
 		animation_player.play("fall_left")
--- 2d-platformer-ahh/Scripts/player.gd
+++ 2d-platformer-ahh/Scripts/player.gd
@@ -7,20 +7,37 @@ class_name PlayerController
 var speed_multiplier = 30.0
 var jump_multiplier = -30.0
 var direction = 0
+var right = true
+var left = false
 
+@export var coyote_time := 0.15
+var coyote_timer := 0.0
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
+	if direction == 1:
+		right = true
+		left = false
+	elif direction == -1:
+		left = true
+		right = false
+
+	if is_on_floor():
+		coyote_timer = coyote_time
+	else:
+		coyote_timer -= delta
+
+	if Input.is_action_just_pressed("Jump") and coyote_timer > 0.0:
+		velocity.y = jump_power * jump_multiplier
+		coyote_timer = 0.0
 
 	direction = Input.get_axis("Move_Left", "Move_Right")
 	if direction:
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
-		
-	if Input.is_action_pressed("Jump") and is_on_floor():
-		velocity.y = jump_power * jump_multiplier
 
 	if Input.is_action_pressed("Down"):
 		set_collision_mask_value(2, false)
